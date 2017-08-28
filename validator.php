a68c5cb5239bee3879c96c782b83cfa5deb57172
Jan Oppolzer <jan.oppolzer@cesnet.cz>
2017-08-28 15:57:59 +0200

    Clean scopeCheck()

--- validator.php
+++ validator.php
@@ -178,7 +178,6 @@ function scopeCheck($metadata) {
 function scopeRegexpCheck ($metadata) {
     $sxe = new SimpleXMLElement (file_get_contents($metadata));
     $sxe->registerXPathNamespace ('shibmd','urn:mace:shibboleth:metadata:1.0');
-    # FIXME: check for <shibmd:Scope> only with IDPs
     $result = $sxe->xpath ('//shibmd:Scope[@regexp]');
     $resultCount = count ($result);
 
