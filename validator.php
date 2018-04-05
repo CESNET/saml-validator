494220f45c315ffdc58cd33b399ed89c0a04efdf
Jan Oppolzer <jan@oppolzer.cz>
2018-04-05 15:19:58 +0200

    Check for XMLwriter closes #16
    In case `xmlwritter` isn't available, throw an exception.

--- validator.php
+++ validator.php
@@ -54,6 +54,14 @@ function writeXML($returncode, $message = null) {
 }
 
 /**
+ * checkDependencies() checks for required PHP dependencies.
+ */
+function checkDependencies() {
+    if(!extension_loaded(xmlwriter))
+        throw new Exception("XMLwritter support not available.");
+}
+
+/**
  * checkTempDir() checks $TMP_DIRECTORY for existance and writtability.
  */
 function checkTempDir($dir) {
@@ -638,6 +646,7 @@ function deleteMetadata($file) {
 /* -------------------------------------------------- */
 
 try {
+    checkDependencies();
     checkTempDir($TMP_DIRECTORY);
 
     $url    = getMetadataURL();
