de30ea8be0bd95f65c7ad8389e4fcef456065422
Jan Oppolzer <jan@oppolzer.cz>
2019-02-02 12:34:33 +0100

    Incremental refactoring

---
+++ functions.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * getRequestMethod() returns the value of $_SERVER["REQUEST_METHOD"].
+ */
+function getRequestMethod() {
+    return $_SERVER["REQUEST_METHOD"];
+}
+
+/**
+ * isGet() returns true if $_SERVER["REQUEST_METHOD"] equals to GET.
+ */
+function isGet() {
+    return strcmp(strtolower(getRequestMethod()), "get") === 0;
+}
+
+/**
+ * isPost() returns true if $_SERVER["REQUEST_METHOD"] equals to POST.
+ */
+function isPost() {
+    return strcmp(strtolower(getRequestMethod()), "post") === 0;
+}
+
+/**
+ * validateURL() returns a valid HTTPS URL or throws an exception.
+ */
+function validateURL($url) {
+    if(!filter_var($url, FILTER_VALIDATE_URL))
+        throw new Exception("Invalid metadata URL.");
+
+    elseif(!preg_match("/^https\:\/\//", $url))
+        throw new Exception("Metadata URL must be HTTPS.");
+
+    return $url;
+}
+
+/**
+ * getVariable() returns value of the non-empty variable or false.
+ */
+function getVariable($var) {
+    return !empty($var) ? $var : false;
+}
+
+/**
+ * getMetadata() returns a link to metadata (either in $_POST["link"] or
+ * $_GET["link"]) or a metadata file ($_FILES["file"]).
+ */
+function getMetadata() {
+    if(isGet()) {
+        $link = @getVariable($_GET["link"]);
+
+        if(!$link)
+            throw new Exception("No metadata URL defined in HTTP GET variable `link`.");
+
+        return validateURL($link);
+    }
+
+    if(isPost()) {
+        $link = @getVariable($_POST["link"]);
+        $file = $_FILES["file"];
+
+        if((empty($link) && $file["size"] === 0))
+            throw new Exception("Neither metadata URL defined nor metadata file selected.");
+
+        elseif(!empty($link) && $file["size"] > 0)
+            throw new Exception("Either define metadata URL or select metadata file, not both.");
+
+        elseif(!empty($link)) {
+            validateURL($link);
+            return $link;
+        }
+
+        elseif($file["size"] > 0)
+            return $file["tmp_name"];
+        }
+
+    return false;
+}
+
--- validate.php
+++ validate.php
@@ -1,63 +1,14 @@
 <?php
 
 /**
+ * Require statements.
+ */
+require_once("./functions.php");
+
+/**
  * Variables.
  */
 $ALLOWED_MIMES = array("text/xml");
-
-/**
- * getRequestMethod() returns the value of $_SERVER["REQUEST_METHOD"].
- */
-function getRequestMethod() {
-    return $_SERVER["REQUEST_METHOD"];
-}
-
-/**
- * isGet() returns true if $_SERVER["REQUEST_METHOD"] equals to GET.
- */
-function isGet() {
-    return strcmp(strtolower(getRequestMethod()), "get") === 0;
-}
-
-/**
- * isPost() returns true if $_SERVER["REQUEST_METHOD"] equals to POST.
- */
-function isPost() {
-    return strcmp(strtolower(getRequestMethod()), "post") === 0;
-}
-
-/**
- * validateURL() returns a valid HTTPS URL or throws an exception.
- */
-function validateURL($url) {
-    if(!filter_var($url, FILTER_VALIDATE_URL))
-        throw new Exception("Invalid metadata URL defined in HTTP GET variable `link`.");
-
-    elseif(!preg_match("/^https\:\/\//", $url))
-        throw new Exception("Metadata URL defined in HTTP GET variable `link` must be HTTPS.");
-
-    return $url;
-}
-
-/**
- * linkOrFile() returns a valid metadata link, the uploaded file or throws an
- * exception.
- */
-function linkOrFile($link, $file) {
-    if((empty($link) && $file["size"] === 0))
-        throw new Exception("Neither metadata URL defined nor metadata file selected.");
-
-    elseif(!empty($link) && $file["size"] > 0)
-        throw new Exception("Either define metadata URL or select metadata file, not both.");
-
-    elseif(!empty($link)) {
-        validateURL($link);
-        return $link;
-    }
-
-    elseif($file["size"] > 0)
-        return $file;
-}
 
 /**
  * All the magic validate.php does is executed here.
@@ -65,25 +16,9 @@ function linkOrFile($link, $file) {
 try {
     echo "<pre>";
 
-    if(isGet()) {
-        $link = !empty($_GET["link"]) ? $_GET["link"] : false;
+    $metadata = getMetadata();
 
-        if(!$link)
-            throw new Exception("No metadata URL defined in HTTP GET variable `link`.");
-
-        validateURL($link);
-    }
-
-    if(isPost()) {
-        $remoteMetadata = linkOrFile($_POST["link"], $_FILES["file"]);
-
-        if(is_array($remoteMetadata))
-            $metadata = $remoteMetadata["tmp_name"];
-        else
-            $metadata = $remoteMetadata;
-    }
-
-    include "./validator.php";
+    require_once("./validator.php");
 
 } catch(Throwable $t) {
     echo "Caught Exception: ", $t->getMessage(), "\n";
